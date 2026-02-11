380f083c19de9012a86d67d1c9ea9f0f2a312ca2
radanpro <radan1pro@gmail.com>
2026-02-12 00:57:41 +0300

    Transform in Canvas sketch

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -3,40 +3,30 @@ const canvasSketch = require("canvas-sketch");
 const settings = {
   dimensions: [1080, 1080],
   // pixelsPerInch: 300,
-  orientation:'landscape'
+  // orientation:'landscape'
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
+    context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-    context.lineWidth = width * 0.01;
+    context.fillStyle = 'black';
 
-    const w = width * 0.1;
-    const h = height * 0.1;
-    const gap = width * 0.03; 
-    const ix = width * 0.17;
-    const iy = height * 0.17;
+    const x = width * 0.5;
+    const y = height * 0.5;
+    const w = width * 0.3;
+    const h = height * 0.3;
 
-    const off = width * 0.2;
+    context.save();
+    context.translate(x, y);
+    context.rotate(0.3);
 
-    let x, y;
-    for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 5; j++) {
-        x = ix + (w + gap) * i;
-        y = iy + (w + gap) * j;
+    context.beginPath();
+    context.rect(-w * 0.5, -h * 0.5, w, h);
+    context.fill();
+    context.restore();
 
-        context.beginPath();
-        context.rect(x, y, w, h);
-        context.stroke();
 
-        if (Math.random() > 0.5) {
-          context.beginPath();
-          context.rect(x + off / 2, y + off / 2, w - off, h - off);
-          context.stroke();
-        }
-      }
-    }
   };
 };
 
